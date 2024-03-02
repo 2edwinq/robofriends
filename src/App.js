28894dcc5015010c269f429d7c9b9ad9a8e051c6
Edwin Quintanilla <2edwinq@gmail.com>
2024-03-01 18:21:52 -0800

    Use the componentdidmount and the error catching function in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
+//
 import SearchBox from './SearchBox';
 import './App.css'
 
@@ -8,9 +8,16 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+
   }
 
   onSearchChange = (event) => {
@@ -21,13 +28,18 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className='tc'>
-        <h1 className='f1 f-sega-log0'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.lenght === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
+
   }
 
 }
